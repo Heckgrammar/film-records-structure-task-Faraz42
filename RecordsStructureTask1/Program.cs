5179d94a520d7b08b24780c224d82edab06cdf1e
Faraz Shariff <21FShariff@heckgrammar.co.uk>
2025-03-06 19:41:59 +0000

    finished homework

--- RecordsStructureTask1/Program.cs
+++ RecordsStructureTask1/Program.cs
@@ -24,15 +24,30 @@
             Film ironMan = new Film("Iron Man","12A",2008,false);
             Film antMan = new Film("Ant-Man", "12A", 2015, false);
             Film[] filmCollection = new Film[] { antMan,hulk,ironMan };
-            int year = 0;
-            int position = 0;
-
             //  Write the code to do the following
             // Loop through the array of films and check for the newest film
             // Produce one output to say the name of the newest film
 
             // Write the code to update the andMan record to show the film is currently being shown
-
+            filmCollection[0].beingShown = true;
+            for (int i = 0; i < filmCollection.Length;)
+            {
+                if (filmCollection[i].year > filmCollection[i + 1].year && filmCollection[i].year > filmCollection[i + 2].year) // checks if the current film is newer than the other two
+                {
+                    Console.WriteLine($"the newest film is {filmCollection[i].title}");
+                    break;
+                }
+                else if (filmCollection[i + 1].year > filmCollection[i + 2].year)
+                {
+                    Console.WriteLine($"the newest film is {filmCollection[i + 1].title}");
+                    break;
+                }
+                else 
+                {
+                    Console.WriteLine($"the newest film is {filmCollection[i + 2].title}");
+                    break;
+                }
+            }
 
         }
     }
